34d193188704ea35caaae008fb733e8a3080402f
Vishal <vishalwebdev0987@gmail.com>
2022-08-19 23:24:15 +0530

    navbar dark mode added, copy and remove extra spaces functionality added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import About from './components/About';
+import { useState } from 'react';
 
 function App() {
+  const [themeMode, setthemeMode] = useState('light');
+
+  const toggleThemeMode = () => {
+    setthemeMode(themeMode === 'light' ? 'dark' : 'light');
+  }
   return (
     <div>
 
-      <Navbar title="TextUtils" aboutText="About TextUtils" />
+      <Navbar title="TextUtils" aboutText="About TextUtils" mode={themeMode} toggleThemeMode={toggleThemeMode} />
       <div className='container my-3'>
         <TextForm heading="Enter the text to analyze below" />
         {/* <About /> */}
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -23,6 +23,15 @@ export default function TextForm(props) {
         settext(event.target.value)
     }
 
+    const handleCopyClick = () => {
+        navigator.clipboard.writeText(text)
+    }
+
+    const handleExtraSpaceRemoveClick = () => {
+        let t = text.replace(/\s+/g, ' ')
+        settext(t)
+    }
+
     return (
         <>
             <div className='container'>
@@ -33,6 +42,8 @@ export default function TextForm(props) {
                 <button className="btn btn-primary" onClick={handleUpClick}>UpperCase</button>
                 <button className="btn btn-primary mx-3" onClick={handleLowerClick}>LowerCase</button>
                 <button className="btn btn-primary" onClick={handleClearClick}>Clear</button>
+                <button className="btn btn-primary mx-3" onClick={handleCopyClick}>Copy</button>
+                <button className="btn btn-primary" onClick={handleExtraSpaceRemoveClick}>Remove Extra Spaces</button>
             </div>
             <div className="container my-3">
                 <h2>Your Text Summary</h2>
